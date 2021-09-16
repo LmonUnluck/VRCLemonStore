e77ff5a3d594f859df07905420018d6851ac1e3d
Zak Lynch <zak.lynch@hotmail.com>
2021-09-16 21:28:20 +1000

    Separate Menu Items

--- Scripts/Lmon Store/LmonStoreMenuItem.cs
+++ Scripts/Lmon Store/LmonStoreMenuItem.cs
@@ -13,8 +13,9 @@ public enum DownloadType
 }
 
 [CreateAssetMenu(fileName = "Item", menuName = "Lmon/New Menu Item", order = 3)]
-public class LmonStoreObject : ScriptableObject
+public class LmonStoreMenuItem : ScriptableObject
 {
+    public const string version = "v1.2";
     public string AssetName;
 
     public string displayText;
@@ -26,16 +27,93 @@ public class LmonStoreObject : ScriptableObject
 
 
     public string gitHubLink = "";
+    public bool removeV = false;
     public string[] linkSegments = new string[0];
     
     public Object targetImage;
 
     public string disableScript = "";
     public bool findDisableScript = false;
+
+    public static Color[] categoryColors = new Color[] { Color.red, Color.blue, Color.green };
+
+    public static bool DisplayLmonAsset(LmonStoreMenuItem target, Rect r, float yOffset, ref int index, Vector2 scrollView)
+    {
+        bool disable = true;
+
+        if (target.findDisableScript)
+        {
+            string[] splitPath = target.disableScript.Split('/');
+            string[] getAssetName = splitPath[splitPath.Length - 1].Split('.');
+            string[] foundAsset = AssetDatabase.FindAssets(getAssetName[0]);
+
+            if (foundAsset.Length > 0)
+            {
+                for (int i = 0; i < foundAsset.Length; i++)
+                {
+                    if (AssetDatabase.GUIDToAssetPath(foundAsset[i]) == target.disableScript)
+                    {
+                        disable = false;
+                        break;
+                    }
+                }
+            }
+
+            EditorGUI.BeginDisabledGroup(disable);
+        }
+
+        Rect buttonDisplay = new Rect(40, (30 * index) + yOffset, r.width - 60, 25);
+
+        if (target.targetImage != null)
+        {
+            if (target.DownloadType == DownloadType.Lmon)
+            {
+                Texture targetTexture = (Texture)target.targetImage;
+
+                GUI.DrawTexture(new Rect(10, ((30 * index)) + yOffset, 25, 25), targetTexture);
+            }
+        }
+        if (target.DownloadType == DownloadType.GitHub)
+        {
+            Texture targetTexture = (Texture)AssetDatabase.LoadAssetAtPath("Assets/Scripts/Lmon Store/Images/GitHub-Mark-Light-64px.png", typeof(Texture));
+            if (!EditorGUIUtility.isProSkin)
+            {
+                targetTexture = (Texture)AssetDatabase.LoadAssetAtPath("Assets/Scripts/Lmon Store/Images/GitHub-Mark-64px.png", typeof(Texture));
+            }
+
+            GUI.DrawTexture(new Rect(10, ((30 * index)) + yOffset, 25, 25), targetTexture);
+        }
+
+        Color defaultColor = GUI.backgroundColor;
+        GUI.backgroundColor = defaultColor * (categoryColors[(int)target.category] * 0.35f);
+        if (GUI.Button(buttonDisplay, "Download " + target.displayText))
+        {
+            return true;
+
+        }
+        GUI.backgroundColor = defaultColor;
+        index++;
+
+        if (target.findDisableScript)
+        {
+            EditorGUI.EndDisabledGroup();
+
+            if (disable)
+            {
+                EditorGUI.HelpBox(new Rect(40, ((30 * index)) + yOffset, r.width - 60, 25), "Install: " + target.disableScript, MessageType.Error);
+            }
+            else
+            {
+                EditorGUI.HelpBox(new Rect(40, ((30 * index)) + yOffset, r.width - 60, 25), target.disableScript + " Installed", MessageType.Info);
+            }
+            index++;
+        }
+        return false;
+    }
 }
 
 #if UNITY_EDITOR
-[CustomEditor(typeof(LmonStoreObject))]
+[CustomEditor(typeof(LmonStoreMenuItem))]
 public class LmonStoreObjectEditor : Editor
 {
     bool arrayToggle = false;
@@ -45,7 +123,7 @@ public class LmonStoreObjectEditor : Editor
 
     public override void OnInspectorGUI()
     {
-        LmonStoreObject lmonObject = (LmonStoreObject)target;
+        LmonStoreMenuItem lmonObject = (LmonStoreMenuItem)target;
 
         lmonObject.AssetName = EditorGUILayout.TextField("Asset Name", lmonObject.AssetName);
         lmonObject.displayText = EditorGUILayout.TextField("Display Text", lmonObject.displayText);
@@ -75,6 +153,10 @@ public class LmonStoreObjectEditor : Editor
         else if (lmonObject.DownloadType == DownloadType.GitHub)
         {
             lmonObject.gitHubLink = EditorGUILayout.TextField("Github link (lastest)", lmonObject.gitHubLink);
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("Remove 'v'");
+            lmonObject.removeV = EditorGUILayout.Toggle(lmonObject.removeV);
+            EditorGUILayout.EndHorizontal();
             arrayToggle = EditorGUILayout.BeginToggleGroup("Link Segments", arrayToggle);
 
 
@@ -146,7 +228,13 @@ public class LmonStoreObjectEditor : Editor
                 {
                     if (i < lmonObject.linkSegments.Length - 1)
                     {
-                        outputString += lmonObject.linkSegments[i] + debugString;
+                        if (lmonObject.removeV)
+                        {
+                            outputString += lmonObject.linkSegments[i] + debugString.Replace("v","");
+                        } else
+                        {
+                            outputString += lmonObject.linkSegments[i] + debugString;
+                        }
                     }
                     else
                     {
